2d12e31f6ace1ff2d23ba4d15dfd452c26047eab
Furpu <feripuru@proton.me>
2024-09-02 10:39:29 -0300

    chore: refactor and generalize Parser code

---
+++ src/Parser.zig
@@ -0,0 +1,138 @@
+const std = @import("std");
+const mem = std.mem;
+
+input: []const u8,
+cursor: usize = 0,
+
+pub const Predicate = *const fn (c: u8) bool;
+
+const Self = @This();
+
+pub fn peek(self: Self) ?u8 {
+    if (self.cursor >= self.input.len) return null;
+    return self.input[self.cursor];
+}
+
+pub fn parseMaybe(self: *Self, b: u8) bool {
+    if (self.peek()) |c| {
+        if (c == b) {
+            self.consume(1);
+            return true;
+        }
+    }
+    return false;
+}
+
+pub fn parseOneOf(self: *Self, bs: []const u8) !u8 {
+    if (self.peek()) |c| {
+        for (bs) |b| {
+            if (c == b) {
+                self.consume(1);
+                return b;
+            }
+        }
+        return error.Parse;
+    } else {
+        return error.EndOfInput;
+    }
+}
+
+pub fn parseString(self: *Self, s: []const u8) !void {
+    if (!mem.startsWith(u8, self.getInput(), s)) return error.Parse;
+    self.consume(s.len);
+}
+
+pub fn parseMany1(self: *Self, pred: Predicate) ![]const u8 {
+    const at = self.getInput();
+
+    var offset: usize = 0;
+    while (self.peek()) |c| {
+        if (!pred(c)) break;
+        self.consume(1);
+        offset += 1;
+    }
+    if (offset == 0) return error.Parse;
+
+    return at[0..offset];
+}
+
+pub fn skip(self: *Self, pred: Predicate) void {
+    while (self.peek()) |c| {
+        if (!pred(c)) break;
+        self.consume(1);
+    }
+}
+
+inline fn getInput(self: Self) []const u8 {
+    return self.input[self.cursor..];
+}
+
+inline fn consume(self: *Self, n: usize) void {
+    self.cursor += n;
+}
+
+test "peek" {
+    const input = "ab";
+
+    var p = Self{ .input = input };
+    try std.testing.expectEqual('a', p.peek());
+
+    p.consume(1);
+    try std.testing.expectEqual('b', p.peek());
+
+    p.consume(1);
+    try std.testing.expectEqual(null, p.peek());
+}
+
+test "parseMaybe" {
+    const input = "ab";
+
+    var p = Self{ .input = input };
+
+    // Should not consume if doesn't match.
+    try std.testing.expectEqual('a', p.peek());
+    try std.testing.expect(!p.parseMaybe('b'));
+    try std.testing.expectEqual('a', p.peek());
+
+    // Should consume if matches
+    try std.testing.expect(p.parseMaybe('a'));
+    try std.testing.expectEqual('b', p.peek());
+}
+
+test "parseOneOf" {
+    const input = "ab";
+
+    var p = Self{ .input = input };
+    try std.testing.expectEqual('a', try p.parseOneOf("kpa"));
+    try std.testing.expectError(error.Parse, p.parseOneOf("acd"));
+}
+
+test "parseString" {
+    const input = "somestrX to parse";
+
+    var p = Self{ .input = input };
+    try p.parseString("somestr");
+    try std.testing.expectEqual('X', p.peek());
+}
+
+fn isAb(c: u8) bool {
+    return c == 'a' or c == 'b';
+}
+
+test "parseMany1" {
+    const input = "ababaabcde";
+
+    var p = Self{ .input = input };
+
+    try std.testing.expectEqualStrings("ababaab", try p.parseMany1(isAb));
+    try p.parseString("cde");
+}
+
+test "skip" {
+    const input = "ababaabcde";
+
+    var p = Self{ .input = input };
+
+    p.skip(isAb);
+    try p.parseString("cde");
+}
--- src/pem.zig
+++ src/pem.zig
@@ -3,48 +3,49 @@ const builtin = @import("builtin");
 const mem = std.mem;
 const ascii = std.ascii;
 
+const Parser = @import("Parser.zig");
+
 const Allocator = mem.Allocator;
 const ArenaAllocator = std.heap.ArenaAllocator;
 
-pub const Parsed = struct {
+pub const Pem = struct {
     arena: *ArenaAllocator,
     label: []const u8,
     msg: []const u8,
 
-    pub fn deinit(self: Parsed) void {
+    pub fn deinit(self: Pem) void {
         self.arena.deinit();
         self.arena.child_allocator.destroy(self.arena);
     }
 };
 
-pub fn parse(allocator: Allocator, input: []const u8) !Parsed {
-    var p = Parser{ .input = input };
+pub fn parse(allocator: Allocator, input: []const u8) !Pem {
+    var p = PemParser{ .inner = .{ .input = input } };
     const parsed = p.parse(allocator) catch |err| {
         // TODO: Remove this and add proper error messages to Parser.
         // I'm putting this here just to aid debugging for now.
-        if (builtin.is_test) std.debug.print("{s}\n", .{p.getInput()});
+        if (builtin.is_test) std.debug.print("\"{s}\"\n", .{p.inner.input[p.inner.cursor..]});
         return err;
     };
 
     return parsed;
 }
 
-const Parser = struct {
-    cursor: usize = 0,
-    input: []const u8,
+const PemParser = struct {
+    inner: Parser,
 
     const b64_decoder = std.base64.standard.Decoder;
     const preeb_start_str = "-----BEGIN ";
     const eb_end_str = "-----";
 
-    pub fn parse(self: *Parser, allocator: Allocator) !Parsed {
-        var parsed = Parsed{ .arena = undefined, .label = undefined, .msg = undefined };
+    pub fn parse(self: *PemParser, allocator: Allocator) !Pem {
+        var parsed = Pem{ .arena = undefined, .label = undefined, .msg = undefined };
         parsed.arena = try allocator.create(ArenaAllocator);
         parsed.arena.* = ArenaAllocator.init(allocator);
         errdefer parsed.deinit();
 
         parsed.label = try self.parsePreeb(parsed.arena.allocator());
-        self.skipWhitespace();
+        self.inner.skip(isWhitespace);
         try self.parseEol();
 
         parsed.msg = try self.parseMsg(parsed.arena.allocator());
@@ -52,26 +53,16 @@ const Parser = struct {
         return parsed;
     }
 
-    fn parsePreeb(self: *Parser, allocator: Allocator) ![]const u8 {
-        try self.parsePreebStart();
+    fn parsePreeb(self: *PemParser, allocator: Allocator) ![]const u8 {
+        try self.inner.parseString(preeb_start_str);
         const label = try self.parseLabel(allocator);
-        try self.parsePreebEnd();
+        try self.inner.parseString(eb_end_str);
 
         return label;
     }
 
-    fn parsePreebStart(self: *Parser) !void {
-        if (!mem.startsWith(u8, self.getInput(), preeb_start_str)) return error.Parse;
-        try self.consume(preeb_start_str.len);
-    }
-
-    fn parsePreebEnd(self: *Parser) !void {
-        if (!mem.startsWith(u8, self.getInput(), eb_end_str)) return error.Parse;
-        try self.consume(eb_end_str.len);
-    }
-
-    fn parseLabel(self: *Parser, allocator: Allocator) ![]const u8 {
-        const label_slice = try self.parseMany(isLabelChar);
+    fn parseLabel(self: *PemParser, allocator: Allocator) ![]const u8 {
+        const label_slice = try self.inner.parseMany1(isLabelChar);
 
         // TODO: rollback cursor on error
         const label = try allocator.alloc(u8, label_slice.len);
@@ -80,15 +71,15 @@ const Parser = struct {
         return label;
     }
 
-    fn parseMsg(self: *Parser, allocator: Allocator) ![]const u8 {
+    fn parseMsg(self: *PemParser, allocator: Allocator) ![]const u8 {
         var buffer = std.ArrayList(u8).init(allocator);
         errdefer buffer.deinit();
 
-        while (true) {
+        while (self.inner.peek()) |c| {
             // Start of posteeb
-            if (self.getInput()[0] == '-') break;
+            if (c == '-') break;
 
-            const line = try self.parseMany(isBase64Char);
+            const line = try self.inner.parseMany1(isBase64Char);
             const slice = try buffer.addManyAsSlice(try b64_decoder.calcSizeForSlice(line));
             try b64_decoder.decode(slice, line);
 
@@ -100,58 +91,13 @@ const Parser = struct {
         return buffer.toOwnedSlice();
     }
 
-    fn parseEol(self: *Parser) !void {
-        const input_start = self.getInput();
-        if (input_start[0] != '\r' and input_start[0] != '\n') return error.Parse;
-
-        if (self.getInput()[0] == '\r') try self.consume(1);
-        if (self.getInput()[0] == '\n') try self.consume(1);
+    fn parseEol(self: *PemParser) !void {
+        const c = try self.inner.parseOneOf("\r\n");
+        if (c == '\r') _ = self.inner.parseMaybe('\n');
     }
 
-    fn parseMany(self: *Parser, pred_fn: *const fn (c: u8) bool) ![]const u8 {
-        var offset: usize = 0;
-        while (true) : (offset += 1) {
-            const input = try self.getInputOffset(offset);
-            if (!pred_fn(input[0])) break;
-        }
-
-        if (offset == 0) return error.Parse;
-
-        const parsed = self.getInput();
-        try self.consume(offset);
-
-        return parsed[0..offset];
-    }
-
-    fn skipWhitespace(self: *Parser) void {
-        while (true) {
-            const input = self.getInput();
-            if (input[0] == '\n' or input[0] == '\r' or !ascii.isWhitespace(input[0])) {
-                break;
-            }
-            self.consume(1) catch break;
-        }
-    }
-
-    fn getInput(self: Parser) []const u8 {
-        return self.getInputOffset(0) catch unreachable;
-    }
-
-    fn getInputOffset(self: Parser, offset: usize) ![]const u8 {
-        const start = self.cursor + offset;
-        if (start >= self.input.len) return error.EndOfInput;
-
-        return self.input[start..];
-    }
-
-    fn consume(self: *Parser, n: usize) !void {
-        const cursor_after = self.cursor + n;
-        if (cursor_after >= self.input.len) return error.EndOfInput;
-        self.cursor = cursor_after;
-    }
-
-    fn rollback(self: *Parser, n: usize) void {
-        self.cursor -= n;
+    fn isWhitespace(c: u8) bool {
+        return c != '\n' and c != '\r' and ascii.isWhitespace(c);
     }
 
     fn isLabelChar(c: u8) bool {
@@ -162,6 +108,10 @@ const Parser = struct {
         return ascii.isAlphanumeric(c) or c == '+' or c == '/';
     }
 };
+
+test {
+    _ = @import("Parser.zig");
+}
 
 test "parse" {
     const pem_str =
