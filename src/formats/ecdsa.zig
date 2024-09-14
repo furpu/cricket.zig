beed63f70c5587fdc67582131172e4f6baa02e90
furpu <feripuru@proton.me>
2024-09-14 04:01:17 -0300

    chore: dynamic sized ECDSA key decoding

--- src/formats/ecdsa.zig
+++ src/formats/ecdsa.zig
@@ -4,7 +4,7 @@ const der = @import("der.zig");
 
 // Named curve OIDs from:
 // https://datatracker.ietf.org/doc/html/rfc5480#section-2.1.1.1
-const secp256r1_oid = der.ObjectIdentifier.fromArcStringComptime("1.2.840.10045.3.1.7");
+pub const secp256r1_oid = der.ObjectIdentifier.fromArcStringComptime("1.2.840.10045.3.1.7");
 
 /// Supported curves.
 pub const Curve = enum {
@@ -18,14 +18,12 @@ pub const EcParameters = union(enum) {
 
 /// Elliptic Curve private key structure as defined in:
 /// https://datatracker.ietf.org/doc/html/rfc5915
-pub fn EcPrivateKey(comptime key_size: usize) type {
-    return struct {
-        version: u8,
-        private_key: [key_size]u8,
-        params: ?der.ContextSpecific(EcParameters, .explicit, 0),
-        public_key: ?der.ContextSpecific(der.BitString, .explicit, 1),
-    };
-}
+pub const EcPrivateKey = struct {
+    version: u8,
+    private_key: []const u8,
+    params: ?der.ContextSpecific(EcParameters, .explicit, 0),
+    public_key: ?der.ContextSpecific(der.BitString, .explicit, 1),
+};
 
 const Pem = @import("Pem.zig");
 
@@ -41,8 +39,10 @@ test "decode" {
     const parsed = try Pem.parse(std.testing.allocator, pem_str);
     defer parsed.deinit();
 
-    const pki = try der.read(EcPrivateKey(32), parsed.msg);
+    const pki = try der.read(EcPrivateKey, parsed.msg);
     try std.testing.expect(pki.params.?.value.named_curve.matches(&secp256r1_oid));
 
-    _ = try std.crypto.sign.ecdsa.EcdsaP256Sha256.SecretKey.fromBytes(pki.private_key);
+    var key_buf: [32]u8 = undefined;
+    @memcpy(&key_buf, pki.private_key);
+    _ = try std.crypto.sign.ecdsa.EcdsaP256Sha256.SecretKey.fromBytes(key_buf);
 }
